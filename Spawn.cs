4f84a1a0ac1424c1df9bffe6642cd142c478f568
christophe kede <chris.kede@yahoo.fr>
2020-12-20 20:16:07 +0100

    First commit

---
+++ Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Generer des spheres
+public class Spawn : MonoBehaviour
+{
+    public Transform[] spawnP;
+    public GameObject[] cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(StartSpawn());
+    }
+   IEnumerator StartSpawn(){
+       yield return new WaitForSeconds(4);
+
+       for(int i =0; i<3; i++){
+           Instantiate(cubes[i], spawnP[i].position, Quaternion.identity);
+       }
+       StartCoroutine(StartSpawn());
+   }
+}
---
+++ scripts/Touch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Supprimer les spheres
+public class Touch : MonoBehaviour
+{
+
+
+    public GameObject arCam;
+
+    public void touch(){
+        RaycastHit hit;
+        if(Physics.Raycast(arCam.transform.position, arCam.transform.forward, out hit)){
+            if(hit.transform.name == "sp1(Clone)" || hit.transform.name == "sp2(Clone)" || hit.transform.name == "sp3(Clone)"){
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
